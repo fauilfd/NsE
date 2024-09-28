20db1c5638f1b673102a082df604915f693cf6fb
溯_fauilfd <su_fauilfd@outlook.com>
2024-09-28 19:52:46 +0800

    基础

---
+++ src/main/java/com/fauilfd/nse/NetherstarextendDataGenerator.java
@@ -0,0 +1,11 @@
+package com.fauilfd.nse;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NetherstarextendDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
